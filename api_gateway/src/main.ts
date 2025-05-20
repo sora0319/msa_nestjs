483f531b4ae1b13d333b849bba5f2472d6f436aa
sora0319 <sojeoung536985@gmail.com>
2025-05-20 11:07:27 +0900

    feat : api gateway 기본 틀틀

---
+++ api_gateway/src/main.ts
@@ -0,0 +1,32 @@
+// api-gateway/src/main.ts
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // /auth
+  app.use(
+    '/auth',
+    createProxyMiddleware({
+      target: 'http://auth-server:3000',
+      changeOrigin: true,
+      pathRewrite: { '^/auth': '' },
+    }),
+  );
+
+  // /event → service2
+  app.use(
+    '/event',
+    createProxyMiddleware({
+      target: 'http://evnet-server:3000',
+      changeOrigin: true,
+      pathRewrite: { '^/event': '' },
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
